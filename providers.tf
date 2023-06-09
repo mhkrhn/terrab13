d4725b3e35557c56b8e0aaa2a12bfc8e1137bdb7
mhkrhn <128690942+mhkrhn@users.noreply.github.com>
2023-06-09 10:38:43 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,10 +6,10 @@ terraform {
 }
 
 provider "azurerm" {
-   subscription_id = var.subscription_id
-   #client_id = var.client_id
-   #client_secret = var.client_secret
-   tenant_id = var.tenant_id
+   subscription_id = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+   client_id = "068d90d3-76e6-42bb-a5dd-fef8b72733db"
+   client_secret = "uUR8Q~W7ZabICcqW-~S3~WpvJmJfri0mUMDAgbeP"
+   tenant_id = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
    skip_provider_registration = true
    features {}
 }
